2d7c386aa3cd1e4ae375abbe5cb8fce932ad2687
Craige McWhirter <craige@mcwhirter.io>
2019-11-23 00:56:29 +1000

    Added xrandr support for 2K screen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -24,6 +24,8 @@ setTransparentHook _ = return (All True)
 main = do
     -- Make sure that HDMI is turned off by default
     spawn "xrandr --output HDMI1 --off"
+    spawn "xrandr --newmode \"2560x1440R\"  241.50  2560 2608 2640 2720  1440 1443 1448 1481 +hsync -vsync"
+    spawn "xrandr --addmode HDMI-1 2560x1440R && xrandr --output eDP-1 --auto --output HDMI-1 --primary --above eDP-1 --mode 2560x1440R"
     -- Launch the composite manager
     spawn "xcompmgr -f -C -n -D 3"
     -- Launch xmobar as my task bar.
@@ -80,7 +82,7 @@ main = do
                 )
             -- Turn on the HDMI-1 port and set it as the secondary display
             , ((mod4Mask .|. shiftMask, xK_h), spawn
-                "xrandr --output eDP-1 --primary --auto --output HDMI-1 --right-of eDP-1 --auto"
+                "xrandr --output eDP-1 --auto --output HDMI-1 --primary --above eDP-1 --mode 2560x1440R"
                 )
             , ((0 , 0x1008FF11), spawn "amixer set Master 2%-") -- XF86AudioLowerVolume
             -- , ((mod4Mask , xK_Down), spawn "amixer set Master 2%-")
